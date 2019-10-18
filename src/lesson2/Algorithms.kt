fb466113fec5b355af22e63431f1438682ca5172
ShadowHaund <edvard.dunijsk@mail.ru>
2019-10-18 12:31:10 +0300

    2 lesson

--- src/lesson2/Algorithms.kt
+++ src/lesson2/Algorithms.kt
@@ -2,6 +2,11 @@
 
 package lesson2
 
+import java.io.File
+import kotlin.math.max
+import kotlin.math.sqrt
+
+
 /**
  * Получение наибольшей прибыли (она же -- поиск максимального подмассива)
  * Простая
@@ -26,10 +31,69 @@ package lesson2
  *
  * В случае обнаружения неверного формата файла бросить любое исключение.
  */
+
+
 fun optimizeBuyAndSell(inputName: String): Pair<Int, Int> {
-    TODO()
+    val readF = File(inputName).bufferedReader().readLines()
+    var maxCurSum = 0
+    var maxGlSum = maxGlobalSum(0, 0, 0)
+    var firstI = 0
+    var secondI = 1
+    val listOfPAndM = mutableListOf<Int>()
+    for (i in 1 until readF.size) {
+        listOfPAndM.add(i - 1, readF[i].toInt() - readF[i - 1].toInt())
+    }
+    for (i in listOfPAndM.indices) {
+        maxCurSum = max(listOfPAndM[i], maxCurSum + listOfPAndM[i])
+
+        if (maxCurSum > maxGlSum.sum) {
+            maxGlSum = maxGlobalSum(maxCurSum, firstI, secondI)
+        }
+        if (maxCurSum > 0) secondI++
+        if (maxCurSum < 0) {
+            maxCurSum = 0
+            firstI = i + 1
+            secondI = firstI + 1
+        }
+    }
+
+    return Pair(maxGlSum.fInd + 1, maxGlSum.sInd + 1)
 }
 
+class maxGlobalSum(sum: Int, fInd: Int, sInd: Int) {
+    var sum = sum
+    var fInd = fInd
+    var sInd = sInd
+}
+/*
+for (i in readF.indices) {
+        for (j in i + 1 until readF.size) {
+            var subS = readF.subList(readF[i].toInt(), readF[j].toInt())
+            for (k in subS.indices) {
+                curSum += subS[k].toInt
+            }
+
+            maxGlSum = max(maxCurSum, maxCurSum + subS)
+
+        }
+    }
+
+*/
+/*
+val readF = File(inputName).bufferedReader().readLines()
+    readF.forEachIndexed { i, _ ->  readF[i].toInt()}
+    var maxCurSum = 0
+    var maxGlSum = 0
+    var firstI = 0
+    var lastI = 0
+    for (i in readF.indices){
+
+        maxCurSum = max(readF[i], maxCurSum + readF[i])
+    }
+
+
+
+*/
 /**
  * Задача Иосифа Флафия.
  * Простая
@@ -80,8 +144,25 @@ fun optimizeBuyAndSell(inputName: String): Pair<Int, Int> {
  * но приветствуется попытка решить её самостоятельно.
  */
 fun josephTask(menNumber: Int, choiceInterval: Int): Int {
-    TODO()
+    val men = mutableListOf<Int>()
+    var step = choiceInterval - 1
+    for (i in 0..menNumber)
+        men.add(i)
+//    for (i in menNumber..0)
+//        for (menNumber ) {
+//            men.remove(men[step])
+//            step = (step + choiceInterval) % men[i]
+//        }
+    return men.sum()
 }
+/*
+if (menNumber == 1)
+        /return 0
+    return if (menNumber > 1) (josephTask(menNumber - 1, choiceInterval) + choiceInterval - 1) % menNumber + 1 else -1
+
+    StackOverflowError
+    Падает когда доходит до for(i in 0..20) на каком-то из шагов
+*/
 
 /**
  * Наибольшая общая подстрока.
@@ -95,8 +176,45 @@ fun josephTask(menNumber: Int, choiceInterval: Int): Int {
  * вернуть ту из них, которая встречается раньше в строке first.
  */
 fun longestCommonSubstring(first: String, second: String): String {
-    TODO()
+    val rezL = List(first.length + 1) { IntArray(second.length + 1) }
+    var max = 0
+    var k = 0
+    for (i in 0..first.length)
+        for (j in 0..second.length) {
+            if (i != 0 && j != 0 && first[i - 1] == second[j - 1])
+                rezL[i][j] = rezL[i - 1][j - 1] + 1
+            else
+                rezL[i][j] = 0
+            if (rezL[i][j] > max) {
+                max = rezL[i][j]
+                k = i
+            }
+        }
+    return if (max == 0)
+        ""
+    else
+        first.substring(k - max, k)
 }
+
+//var lenght = 0
+//if (first.length > second.length)
+//while (lenght < second.length - 1) {
+//    for (i in first.indices)
+//        if (second[lenght] in first) {
+//            rezString += second[i]
+//            lenght++
+//        } else lenght++
+//    if (second[i - 1] in first && second[i] !in first) {
+//        lenght = 0
+//    }
+//}
+//else while (lenght < first.length - 1) {
+//    for (i in second.indices)
+//        if (first[lenght] in second) {
+//            rezString += first[i]
+//            lenght ++
+//        } else lenght++
+//}
 
 /**
  * Число простых чисел в интервале
@@ -109,8 +227,70 @@ fun longestCommonSubstring(first: String, second: String): String {
  * Единица простым числом не считается.
  */
 fun calcPrimesNumber(limit: Int): Int {
-    TODO()
+    var num = 0
+    if (limit <= 1)
+        return 0
+    for (i in 2..limit)
+        if (ifIsPrime(i))
+            num++
+    return num
 }
+
+fun ifIsPrime(n: Int): Boolean {
+    val s = sqrt(n.toDouble()).toInt()
+    for (i in 2..s) {
+        if (n % i == 0)
+            return false
+    }
+    return true
+}
+/*
+
+val rezL = mutableListOf<Int>()
+    if (limit <= 1)
+        return 0
+    for (i in 0..limit)
+        if (i == 2 || i != 1 && i % 2 != 0)
+            rezL.add(i)
+    for (i in 3 until rezL.size) {
+        if (i == rezL.size) break
+        for (j in 1..i / 2 + 1)
+            while (2 * rezL[j] <= rezL[i])
+                if (rezL[i] % rezL[j] == 0)
+                    rezL.remove(rezL[i])
+                else break
+    }
+
+    return rezL.size
+}
+
+*/
+/*
+while ((2 * i + 1) * (2 * i + 1) <= (2 * limit + 1) * 2) {
+        if (rezL[i] == 1) {
+            while (j <= limit) {
+                rezL[j] = 0
+                j += 2 * i + 1
+            }
+        }
+        i++
+
+         var dev = 3
+        var numOfDev = 0
+        while (dev <= rezL[i]) {
+            dev += if (rezL[i] % dev == 0) {
+                numOfDev++
+                2
+            } else {
+                2
+            }
+        }
+        if (numOfDev != 1)
+            rezL.remove(rezL[i])
+    }
+    }
+ */
+
 
 /**
  * Балда
